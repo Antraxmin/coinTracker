56a4eb420eebc410cdbbcbc9ff7fbde256af7be5
Antraxmin <antraxmin@naver.com>
2022-10-29 21:33:44 +0900

    feat: fetch coin data from API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
+  const [loading, setLoading] = useState(1);  // initial loading state
+  const [coins, setCoins] = useState([]);     // coin object state
+  useEffect(()=>{
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((response)=>response.json())
+    .then((json)=>{
+      setCoins(json);
+      setLoading(false);
+    });
+  }, []);
   return (
     <div>
     </div>
